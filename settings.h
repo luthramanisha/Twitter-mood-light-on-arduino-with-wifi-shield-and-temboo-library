916a247d9bd8c904347d32955dcc133e9381316c
manishaluthra <manishaluthra247@gmail.com>
2016-01-21 18:02:53 +0100

    settings file for temboo api

---
+++ settings.h
@@ -0,0 +1,10 @@
+-aYOUR_USER_NAME
+-c/Library/Twitter/Search/Tweets
+-umyFirstApp
+-p06fb70a0f22c4540aef359d05e080a87
+-iAccessToken:YOUR_ACCESS_TOKEN
+-iAccessTokenSecret:YOUR_ACCESS_TOKEN_SECRET
+-iConsumerSecret:YOUR_CONSUMER_SECRET
+-iConsumerKey:YOUR_CONSUMER_KEY
+-w/mnt/sda1/settings.h
+-r/mnt/sda1/settings.h
